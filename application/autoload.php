14860f793a292fe24a1c04dd5edf2c1c49881e9f
Steven Surowiec <steven.surowiec@gmail.com>
2010-11-13 17:38:20 +0000

    Initial setup

--- application/autoload.php
+++ application/autoload.php
@@ -7,10 +7,10 @@ $loader->registerNamespaces(array(
 	'Symfony'      => __DIR__ . '/../vendor/symfony/src',
 	'Doctrine\ODM' => __DIR__ . '/../vendor/mongodb-odm/lib',
 	'Bundle'       => __DIR__ . '/../src',
+	'Zend'         => __DIR__ . '/../vendor/zend/library',
 	));
 
 $loader->registerPrefixes(array(
-	'Zend_' => __DIR__ . '/../vendor/zend/library',
 	'Twig_' => __DIR__ . '/../vendor/twig/lib',
 	));
 
