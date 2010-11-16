1b3a4275e381385e76a76ed29b17aed3b63eaed1
Steven Surowiec <steven.surowiec@gmail.com>
2010-11-16 03:11:44 +0000

    Added doctrine common and dbal to autoloader

--- application/autoload.php
+++ application/autoload.php
@@ -4,10 +4,12 @@ require_once __DIR__ . '/../vendor/symfony/src/Symfony/Component/HttpFoundation/
 
 $loader = new \Symfony\Component\HttpFoundation\UniversalClassLoader();
 $loader->registerNamespaces(array(
-	'Symfony'      => __DIR__ . '/../vendor/symfony/src',
-	'Doctrine\ODM' => __DIR__ . '/../vendor/mongodb-odm/lib',
-	'Bundle'       => __DIR__ . '/../src',
-	'Zend'         => __DIR__ . '/../vendor/zend/library',
+	'Symfony'       => __DIR__ . '/../vendor/symfony/src',
+	'Doctrine\DBAL' => __DIR__ . '/../vendor/dctorine-dbal/lib',
+	'Doctrine\ODM'  => __DIR__ . '/../vendor/mongodb-odm/lib',
+	'Doctrine'      => __DIR__ . '/../vendor/doctrine-common/lib',
+	'Bundle'        => __DIR__ . '/../src',
+	'Zend'          => __DIR__ . '/../vendor/zend/library',
 	));
 
 $loader->registerPrefixes(array(
