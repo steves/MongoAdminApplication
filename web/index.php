efdb88a9619a3041de638c8ca947086aca830ed4
Steven Surowiec <steven.surowiec@gmail.com>
2010-11-20 14:49:00 +0000

    Added request instance to handle() call

--- web/index.php
+++ web/index.php
@@ -2,5 +2,7 @@
 
 require_once __DIR__ . '/../application/MongoAdminKernel.php';
 
+use Symfony\Component\HttpFoundation\Request;
+
 $kernel = new MongoAdminKernel('prod', true);
-$kernel->handle()->send();
+$kernel->handle(new Request())->send();
