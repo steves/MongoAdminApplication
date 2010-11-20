eabdf00ae06c187d921f467e87818fd3a211d2cc
Steven Surowiec <steven.surowiec@gmail.com>
2010-11-20 00:09:08 +0000

    Updated autoloader

--- application/autoload.php
+++ application/autoload.php
@@ -4,16 +4,17 @@ require_once __DIR__ . '/../vendor/symfony/src/Symfony/Component/HttpFoundation/
 
 $loader = new \Symfony\Component\HttpFoundation\UniversalClassLoader();
 $loader->registerNamespaces(array(
-	'Symfony'       => __DIR__ . '/../vendor/symfony/src',
-	'Doctrine\DBAL' => __DIR__ . '/../vendor/dctorine-dbal/lib',
-	'Doctrine\ODM'  => __DIR__ . '/../vendor/mongodb-odm/lib',
-	'Doctrine'      => __DIR__ . '/../vendor/doctrine-common/lib',
-	'Bundle'        => __DIR__ . '/../src',
-	'Zend'          => __DIR__ . '/../vendor/zend/library',
-	));
+	'Symfony'         => __DIR__ . '/../vendor/symfony/src',
+	'Doctrine\DBAL'   => __DIR__ . '/../vendor/dctorine-dbal/lib',
+	'Doctrine\ODM'    => __DIR__ . '/../vendor/mongodb-odm/lib',
+	'Doctrine\Common' => __DIR__ . '/../vendor/doctrine-common/lib',
+	'Doctrine'        => __DIR__ . '/../vendor/doctrine/lib',
+	'Bundle'          => __DIR__ . '/../src',
+	'Zend'            => __DIR__ . '/../vendor/zend/library',
+));
 
 $loader->registerPrefixes(array(
 	'Twig_' => __DIR__ . '/../vendor/twig/lib',
-	));
+));
 
 $loader->register();
